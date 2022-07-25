2d8373a3ef11c788e3da0646ca3ace441ee939a1
Tiago Fonseca <tiagorfonseca@ua.pt>
2022-07-25 13:43:13 +0100

    Updating CompressionScript and FASTA_ANALY menu

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# Eficient_DNA_Compressor
+## Eficient_DNA_Compressor
 
 The objective of this project is to find an improved way to compress files with genomic sequences (fasta,fastq,fa,etc). For that we already have a plentitude of compressing tools available in the market, such as NAF,MBGC,GZIP, among others.
 
 But, if we can work on a previous ordered file, where we group the most similar sequences, it's reasonable to think, the end result will also improve. So, to achieve that, we'll run a C++ program dedicated to the order that type of file. The criteria used to do that can be decided by the user when he is running ./FASTA_ANALY. It can go from nucleotide pairs (AT or CG) to size and alphabetical order. Different types of criteria can be grouped to achieve better results.
 
 On the other hand, there's also a compression script being developed where we test 4 different scenarios to understand what works best in compression. One where we just compress the original files, other where we order the sequences and then compress, another where we shuffle the sequences and then compress and finally, one where we mix the previous two strategies.
+
+# Usage Example
+` .\FASTA_ANALY -sort=AT 5 ordered_sequences_virus.fasta sequences_virus.fasta `
+
+A description of the options available can be obtained by invoking:
+
+` ./FASTA_ANALY -h `
--- src/CompressionScript.sh
+++ src/CompressionScript.sh
@@ -2,11 +2,12 @@
 #!/usr/bin/bash
 
 # Scenario 1 - Compress original file (sequences_virus.fasta)
-
+time {
 rm naf_times.txt
 rm mbgctimes.txt
 rm gzip_times.txt
 rm mf_compresstimes.txt
+rm stage1_time.txt
 
 
 { time ennaf --strict  sequences_virus.fasta -o sequences_virus.naf --temp-dir /tmp ; } 2>>naf_times.txt
@@ -18,24 +19,32 @@ rm mf_compresstimes.txt
 { time gzip -k sequences_virus.fasta ; } 2>>gzip_times.txt
 
 
-ls -la -ltr | grep \.naf$ |awk '{print $5;}' > naf_files.txt
-ls -la -ltr | grep \.mbgc$ |awk '{print $5;}' > mbgc_files.txt
-ls -la -ltr | grep \.gz$ |awk '{print $5;}' > gzip_files.txt
-ls -la -ltr | grep \.mfc$ |awk '{print $5;}' > mfc_files.txt
+#ls -la -ltr | grep \.naf$ |awk '{print $5;}' > naf_files.txt
+#ls -la -ltr | grep \.mbgc$ |awk '{print $5;}' > mbgc_files.txt
+#ls -la -ltr | grep \.gz$ |awk '{print $5;}' > gzip_files.txt
+#ls -la -ltr | grep \.mfc$ |awk '{print $5;}' > mfc_files.txt
 
-awk '{print $1}' OFS="|" naf_files.txt > output_naf.csv
-awk '{print $1}' OFS="|" mbgc_files.txt > output_mbgc.csv
-awk '{print $1}' OFS="|" gzip_files.txt > outfile_gzip.csv
-awk '{print $1}' OFS="|" mfc_files.txt > outfile_mfc.csv
+#ls sequences_virus* -la -ltr | grep \.naf$ |awk '{print $5;}' > naf_files.txt
+#ls sequences_virus* -la -ltr | grep \.mbgc$ |awk '{print $5;}' > mbgc_files.txt
+#ls sequences_virus* -la -ltr | grep \.gz$ |awk '{print $5;}' > gzip_files.txt
+#ls sequences_virus* -la -ltr | grep \.mfc$ |awk '{print $5;}' > mfc_files.txt
+
+#awk '{print $1}' OFS="|" naf_files.txt > output_naf.csv
+#awk '{print $1}' OFS="|" mbgc_files.txt > output_mbgc.csv
+#awk '{print $1}' OFS="|" gzip_files.txt > outfile_gzip.csv
+#awk '{print $1}' OFS="|" mfc_files.txt > outfile_mfc.csv
+
+ls sequences_virus* -la -ltr | awk 'BEGIN{ OFS=","; print "File;Size,"}; NR > 1{print $9,$5;}' > Output.csv
+}
+
+#awk  '{print $10,$6,"a"," b","c";}' OFS="," > Results.csv
+
 
 #Scenario 2 - Shuffle Input File and then Compress
 
-#Shuffle Operation Execution Time
-#real	32m30,264s
-#user	17m6,607s
-#sys	15m13,847s
-
-{ ./SHUFFLE_FASTA AT 5 shuffled.fasta sequences_virus.fasta ; } 2>>shuffle_times.txt
+time {
+#{ ./SHUFFLE_FASTA AT 5 shuffled.fasta sequences_virus.fasta ; } 2>>shuffle_times.txt
+{ ./FASTA_ANALY -s AT 5 shuffled.fasta sequences_virus.fasta ; } 2>>shuffle_times.txt
 { time ennaf --strict  shuffled.fasta -o shuffled.naf --temp-dir /tmp ; } 2>>naf_times_s2.txt
 
 { time mbgc -i shuffled.fasta shuffled.mbgc ; } 2>>mbgctimes_s2.txt
@@ -44,10 +53,15 @@ awk '{print $1}' OFS="|" mfc_files.txt > outfile_mfc.csv
 
 { time gzip -k shuffled.fasta ; } 2>>gzip_times_s2.txt
 
-ls -la -ltr | grep \.naf$ |awk '{print $5;}' > naf_files_s2.txt
-ls -la -ltr | grep \.mbgc$ |awk '{print $5;}' > mbgc_files_s2.txt
-ls -la -ltr | grep \.gz$ |awk '{print $5;}' > gzip_files_s2.txt
-ls -la -ltr | grep \.mfc$ |awk '{print $5;}' > mfc_files_s2.txt
+#ls -la -ltr | grep \.naf$ |awk '{print $5;}' > naf_files_s2.txt
+#ls -la -ltr | grep \.mbgc$ |awk '{print $5;}' > mbgc_files_s2.txt
+#ls -la -ltr | grep \.gz$ |awk '{print $5;}' > gzip_files_s2.txt
+#ls -la -ltr | grep \.mfc$ |awk '{print $5;}' > mfc_files_s2.txt
+
+ls sequences_virus* -la -ltr | awk 'BEGIN{ OFS=","; print "File;Size,"}; NR > 1{print $9,$5;}' > Output_s2.csv
+
+}
+
 
 #Scenario 3 - Order Input File and then Compress
 
@@ -59,10 +73,11 @@ ls -la -ltr | grep \.mfc$ |awk '{print $5;}' > mfc_files_s2.txt
 
 { time gzip -k ordered_sequences_virus.fasta ; } 2>>gzip_times_s3.txt
 
-ls -la -ltr | grep \.naf$ |awk '{print $5;}' > naf_files_s3.txt
-ls -la -ltr | grep \.mbgc$ |awk '{print $5;}' > mbgc_files_s3.txt
-ls -la -ltr | grep \.gz$ |awk '{print $5;}' > gzip_files_s3.txt
-ls -la -ltr | grep \.mfc$ |awk '{print $5;}' > mfc_files_s3.txt
+#ls -la -ltr | grep \.naf$ |awk '{print $5;}' > naf_files_s3.txt
+#ls -la -ltr | grep \.mbgc$ |awk '{print $5;}' > mbgc_files_s3.txt
+#ls -la -ltr | grep \.gz$ |awk '{print $5;}' > gzip_files_s3.txt
+#ls -la -ltr | grep \.mfc$ |awk '{print $5;}' > mfc_files_s3.txt
+ls sequences_virus* -la -ltr | awk 'BEGIN{ OFS=","; print "File;Size,"}; NR > 1{print $9,$5;}' > Output_s3.csv
 
 #Scenario 4 - Shuffle then Order then Compress
 
@@ -75,6 +90,8 @@ ls -la -ltr | grep \.mfc$ |awk '{print $5;}' > mfc_files_s3.txt
 { time ./MFCompressC ordered_shuffled.fasta ; } 2>>mf_compresstimes_s4.txt
 
 { time gzip -k ordered_shuffled.fasta ; } 2>>gzip_times_s4.txt
+
+ls sequences_virus* -la -ltr | awk 'BEGIN{ OFS=","; print "File;Size,"}; NR > 1{print $9,$5;}' > Output_s4.csv
 
 
 ls -la -ltr | grep \.naf$ |awk '{print $5;}' > naf_files_s4.txt
