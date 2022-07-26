32e5af229da1912063851f88f2ac4ddcdfcef1de
Tiago Fonseca <tiagorfonseca@ua.pt>
2022-07-26 12:53:38 +0100

    Updating CompressionScript

--- src/CompressionScript.sh
+++ src/CompressionScript.sh
@@ -8,6 +8,10 @@ rm mbgctimes.txt
 rm gzip_times.txt
 rm mf_compresstimes.txt
 rm stage1_time.txt
+rm sequences_virus.fasta.gz
+rm sequences_virus.fasta.mfc
+rm sequences_virus.mbgc
+rm sequences_virus.naf
 
 
 { time ennaf --strict  sequences_virus.fasta -o sequences_virus.naf --temp-dir /tmp ; } 2>>naf_times.txt
@@ -34,7 +38,61 @@ rm stage1_time.txt
 #awk '{print $1}' OFS="|" gzip_files.txt > outfile_gzip.csv
 #awk '{print $1}' OFS="|" mfc_files.txt > outfile_mfc.csv
 
-ls sequences_virus* -la -ltr | awk 'BEGIN{ OFS=","; print "File;Size,"}; NR > 1{print $9,$5;}' > Output.csv
+#valgrind
+#perfis de ordenação
+#cg
+
+declare -a times_arr=()
+
+#times_arr+=$(awk '{print $2}' OFS="|" naf_files.txt)
+#while read line; do
+#    times_arr+=($line)
+#done < naf$_times.txt
+awk 'FNR == 2 {print $2}' naf_times.txt > naf_time_total.txt
+awk 'FNR == 2 {print $2}' mbgctimes.txt > mbgc_time_total.txt
+awk 'FNR == 2 {print $2}' mf_compresstimes.txt > mfcompress_time_total.txt
+awk 'FNR == 2 {print $2}' gzip_times.txt > gzip_time_total.txt
+
+
+
+
+#times_arr+=$(awk 'FNR == 2 {print $2}' mbgctimes.txt)
+#times_arr+=$(awk 'FNR == 2 {print $2}' mf_compresstimes.txt)
+#times_arr+=$(awk 'FNR == 2 {print $2}' gzip_times.txt)
+
+while read line; do
+   times_arr+=($line)
+done < naf_time_total.txt
+while read line; do
+   times_arr+=($line)
+done < mbgc_time_total.txt
+while read line; do
+    times_arr+=($line)
+done < mfcompress_time_total.txt
+while read line; do
+    times_arr+=($line)
+done < gzip_time_total.txt
+
+echo "${#times_arr[@]}"
+
+
+declare -p times_arr
+#for i in ${!fasta_arr[@]}; do
+    
+ #   time echo "$((${fasta_arr[i]}))"
+ # done  
+
+time_naf=$(awk '{print $1}' mbgc_time_total.txt)
+#awk '{print $(time_naf)}'
+#echo $((time_naf))
+ls sequences_virus* -la -ltr | awk 'BEGIN{ OFS=","; print "File;Size;Time,"}; NR > 1{print $9,$5;}' > Output.csv
+for i in ${!times_arr[@]}; do
+    
+   echo "$((${fasta_arr[i]}))" | awk 'NR>3{print $1;}' > Output_n.csv
+   
+  done  
+ 
+
 }
 
 #awk  '{print $10,$6,"a"," b","c";}' OFS="," > Results.csv
@@ -58,14 +116,16 @@ time {
 #ls -la -ltr | grep \.gz$ |awk '{print $5;}' > gzip_files_s2.txt
 #ls -la -ltr | grep \.mfc$ |awk '{print $5;}' > mfc_files_s2.txt
 
-ls sequences_virus* -la -ltr | awk 'BEGIN{ OFS=","; print "File;Size,"}; NR > 1{print $9,$5;}' > Output_s2.csv
+ls shuffled.* -la -ltr | awk 'BEGIN{ OFS=","; print "File;Size,"}; NR > 1{print $9,$5;}' > Output_s2.csv
 
 }
 
 
 #Scenario 3 - Order Input File and then Compress
 
-{ time ./FASTA_ANALY AT 5 ordered_sequences_virus.fasta sequences_virus.fasta ; } 2>>ordering_times.txt
+#{ time ./FASTA_ANALY AT 5 ordered_sequences_virus.fasta sequences_virus.fasta ; } 2>>ordering_times.txt
+time {
+{ time ./FASTA_ANALY -sort=size ordered_sequences_virus.fasta sequences_virus.fasta ; } 2>>ordering_times.txt
 { time ennaf --strict  ordered_sequences_virus.fasta -o ordered_sequences_virus.naf --temp-dir /tmp ; } 2>>naf_times_s3.txt
 { time mbgc -i ordered_sequences_virus.fasta ordered_sequences_virus.mbgc ; } 2>>mbgctimes_s3.txt
 
@@ -78,12 +138,14 @@ ls sequences_virus* -la -ltr | awk 'BEGIN{ OFS=","; print "File;Size,"}; NR > 1{
 #ls -la -ltr | grep \.gz$ |awk '{print $5;}' > gzip_files_s3.txt
 #ls -la -ltr | grep \.mfc$ |awk '{print $5;}' > mfc_files_s3.txt
 ls sequences_virus* -la -ltr | awk 'BEGIN{ OFS=","; print "File;Size,"}; NR > 1{print $9,$5;}' > Output_s3.csv
+} 
 
 #Scenario 4 - Shuffle then Order then Compress
 
-{ time ./SHUFFLE_FASTA AT 5 shuffled.fasta sequences_virus.fasta ; } 2>>shuffle_times_s4.txt
-{ time ./FASTA_ANALY AT 5 ordered_shuffled.fasta shuffled.fasta ; } 2>>ordering_times_s4.txt
-
+#{ time ./SHUFFLE_FASTA AT 5 shuffled.fasta sequences_virus.fasta ; } 2>>shuffle_times_s4.txt
+#{ time ./FASTA_ANALY AT 5 ordered_shuffled.fasta shuffled.fasta ; } 2>>ordering_times_s4.txt
+{ ./FASTA_ANALY -s AT 5 shuffled.fasta sequences_virus.fasta ; } 2>>shuffle_times_s4.txt
+{ time ./FASTA_ANALY -sort=size ordered_sequences_virus.fasta sequences_virus.fasta ; } 2>>ordering_times_s4.txt
 { time ennaf --strict  ordered_shuffled.fasta -o ordered_shuffled.naf --temp-dir /tmp ; } 2>>naf_times_s4.txt
 { time mbgc -i ordered_shuffled.fasta ordered_shuffled.mbgc ; } 2>>mbgctimes_s4.txt
 
