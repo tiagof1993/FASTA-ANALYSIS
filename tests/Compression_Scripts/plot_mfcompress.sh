c8d12e75b0bfd216fbdb503612b8474748261e40
unknown <tiagorfonseca@ua.pt>
2023-09-19 11:17:38 +0100

    MFCompress script and plots updated

--- tests/Compression_Scripts/plot_mfcompress.sh
+++ tests/Compression_Scripts/plot_mfcompress.sh
@@ -4,8 +4,8 @@
   #rm *.csv
 partitions_array=("1" "4" "8")
 #MFCOMPRESS_CSV="data_mfcompress"
-NAF_synthetic_CSV=$1
-NAF_real_CSV=$2
+MFCOMPRESS_synthetic_CSV=$1
+MFCOMPRESS_real_CSV=$2
 SORTING_TYPE=$3
   # IN_FILE_SHORT_NAME=$(ls -1 $IN_FILE | sed 's/.fasta//g')
   # echo $IN_FILE_SHORT_NAME
@@ -13,59 +13,59 @@ SORTING_TYPE=$3
 
 #for ((i=0; i<${#MFCOMPRESS_CSV[@]}; i++)); do
 #Build CSV for each partition
- cat data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE.csv | grep -w "1" | awk -F, '$3==1'  > data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-1.csv
- cat data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE.csv| grep -w "4" | awk -F, '$3==4' > data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-4.csv
- cat data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE.csv | grep -w "8" | awk -F, '$3==8' > data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-8.csv
- cat data_mfcompress-$NAF_real_CSV-$SORTING_TYPE.csv | grep -w "1" | awk -F, '$3==1'  > data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-1.csv
- cat data_mfcompress-$NAF_real_CSV-$SORTING_TYPE.csv| grep -w "4" | awk -F, '$3==4' > data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-4.csv
- cat data_mfcompress-$NAF_real_CSV-$SORTING_TYPE.csv | grep -w "8" | awk -F, '$3==8' > data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-8.csv
+ cat data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE.csv | grep -w "1" | awk -F, '$3==1'  > data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-1.csv
+ cat data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE.csv| grep -w "4" | awk -F, '$3==4' > data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-4.csv
+ cat data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE.csv | grep -w "8" | awk -F, '$3==8' > data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-8.csv
+ cat data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE.csv | grep -w "1" | awk -F, '$3==1'  > data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-1.csv
+ cat data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE.csv| grep -w "4" | awk -F, '$3==4' > data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-4.csv
+ cat data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE.csv | grep -w "8" | awk -F, '$3==8' > data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-8.csv
 
 #Build CSV for each sorting algorithm
-cat data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE.csv | grep -e "fasta_analysis" > data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-fasta_analysis.csv
-cat data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE.csv| grep -e "sortmf" > data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-sortmf.csv
-cat data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE.csv | grep -v -e "fasta_analysis" | grep -v -e "sortmf" | grep -e "mfcompress" > data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-not_sorted.csv
+cat data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE.csv | grep -e "fasta_analysis" > data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-fasta_analysis.csv
+cat data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE.csv| grep -e "sortmf" > data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-sortmf.csv
+cat data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE.csv | grep -v -e "fasta_analysis" | grep -v -e "sortmf" | grep -e "mfcompress" > data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-not_sorted.csv
 
-cat data_mfcompress-$NAF_real_CSV-$SORTING_TYPE.csv | grep -e "fasta_analysis" > data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-fasta_analysis.csv
-cat data_mfcompress-$NAF_real_CSV-$SORTING_TYPE.csv| grep -e "sortmf" > data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-sortmf.csv
-cat data_mfcompress-$NAF_real_CSV-$SORTING_TYPE.csv | grep -v -e "fasta_analysis" | grep -v -e "sortmf" | grep -e "mfcompress" > data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-not_sorted.csv
+cat data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE.csv | grep -e "fasta_analysis" > data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-fasta_analysis.csv
+cat data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE.csv| grep -e "sortmf" > data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-sortmf.csv
+cat data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE.csv | grep -v -e "fasta_analysis" | grep -v -e "sortmf" | grep -e "mfcompress" > data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-not_sorted.csv
 
 
 #Build CSV combining partition with sorting algorithm
 #MFCOMPRESS_CSV="data_jarvis3"
- cat data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-1.csv | grep -e "sortmf" > data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-1_sortmf.csv
- echo "$(sort -t$',' -n -k 9 data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-1_sortmf.csv)" > "data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-1_sortmf.csv"
- cat data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-4.csv | grep -e "sortmf" > data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-4_sortmf.csv
-  echo "$(sort -t$',' -n -k 9 data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-4_sortmf.csv)" > "data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-4_sortmf.csv"
- cat data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-8.csv | grep -e "sortmf" > data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-8_sortmf.csv
-  echo "$(sort -t$',' -n -k 9 data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-8_sortmf.csv)" > "data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-8_sortmf.csv"
- cat data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-1.csv | grep -e "fasta_analysis" > data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-1_fasta_analysis.csv
- echo "$(sort -t$',' -n -k 9 data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-1_fasta_analysis.csv)" > "data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-1_fasta_analysis.csv"
- cat data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-4.csv | grep -e "fasta_analysis" > data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-4_fasta_analysis.csv
- echo "$(sort -t$',' -n -k 9 data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-4_fasta_analysis.csv)" > "data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-4_fasta_analysis.csv"
- cat data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-8.csv | grep -e "fasta_analysis" > data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-8_fasta_analysis.csv
- echo "$(sort -t$',' -n -k 9 data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-8_fasta_analysis.csv)" > "data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-8_fasta_analysis.csv"
+ cat data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-1.csv | grep -e "sortmf" > data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-1_sortmf.csv
+ echo "$(sort -t$',' -n -k 9 data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-1_sortmf.csv)" > "data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-1_sortmf.csv"
+ cat data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-4.csv | grep -e "sortmf" > data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-4_sortmf.csv
+  echo "$(sort -t$',' -n -k 9 data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-4_sortmf.csv)" > "data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-4_sortmf.csv"
+ cat data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-8.csv | grep -e "sortmf" > data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-8_sortmf.csv
+  echo "$(sort -t$',' -n -k 9 data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-8_sortmf.csv)" > "data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-8_sortmf.csv"
+ cat data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-1.csv | grep -e "fasta_analysis" > data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-1_fasta_analysis.csv
+ echo "$(sort -t$',' -n -k 9 data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-1_fasta_analysis.csv)" > "data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-1_fasta_analysis.csv"
+ cat data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-4.csv | grep -e "fasta_analysis" > data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-4_fasta_analysis.csv
+ echo "$(sort -t$',' -n -k 9 data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-4_fasta_analysis.csv)" > "data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-4_fasta_analysis.csv"
+ cat data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-8.csv | grep -e "fasta_analysis" > data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-8_fasta_analysis.csv
+ echo "$(sort -t$',' -n -k 9 data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-8_fasta_analysis.csv)" > "data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-8_fasta_analysis.csv"
 
- cat data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-1.csv | grep -e "sortmf" > data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-1_sortmf.csv
- echo "$(sort -t$',' -n -k 9 data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-1_sortmf.csv)" > "data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-1_sortmf.csv"
- cat data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-4.csv | grep -e "sortmf" > data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-4_sortmf.csv
-  echo "$(sort -t$',' -n -k 9 data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-4_sortmf.csv)" > "data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-4_sortmf.csv"
- cat data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-8.csv | grep -e "sortmf" > data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-8_sortmf.csv
-  echo "$(sort -t$',' -n -k 9 data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-8_sortmf.csv)" > "data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-8_sortmf.csv"
- cat data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-1.csv | grep -e "fasta_analysis" > data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-1_fasta_analysis.csv
- echo "$(sort -t$',' -n -k 9 data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-1_fasta_analysis.csv)" > "data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-1_fasta_analysis.csv"
- cat data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-4.csv | grep -e "fasta_analysis" > data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-4_fasta_analysis.csv
- echo "$(sort -t$',' -n -k 9 data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-4_fasta_analysis.csv)" > "data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-4_fasta_analysis.csv"
- cat data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-8.csv | grep -e "fasta_analysis" > data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-8_fasta_analysis.csv
- echo "$(sort -t$',' -n -k 9 data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-8_fasta_analysis.csv)" > "data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-8_fasta_analysis.csv"
+ cat data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-1.csv | grep -e "sortmf" > data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-1_sortmf.csv
+ echo "$(sort -t$',' -n -k 9 data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-1_sortmf.csv)" > "data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-1_sortmf.csv"
+ cat data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-4.csv | grep -e "sortmf" > data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-4_sortmf.csv
+  echo "$(sort -t$',' -n -k 9 data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-4_sortmf.csv)" > "data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-4_sortmf.csv"
+ cat data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-8.csv | grep -e "sortmf" > data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-8_sortmf.csv
+  echo "$(sort -t$',' -n -k 9 data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-8_sortmf.csv)" > "data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-8_sortmf.csv"
+ cat data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-1.csv | grep -e "fasta_analysis" > data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-1_fasta_analysis.csv
+ echo "$(sort -t$',' -n -k 9 data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-1_fasta_analysis.csv)" > "data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-1_fasta_analysis.csv"
+ cat data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-4.csv | grep -e "fasta_analysis" > data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-4_fasta_analysis.csv
+ echo "$(sort -t$',' -n -k 9 data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-4_fasta_analysis.csv)" > "data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-4_fasta_analysis.csv"
+ cat data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-8.csv | grep -e "fasta_analysis" > data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-8_fasta_analysis.csv
+ echo "$(sort -t$',' -n -k 9 data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-8_fasta_analysis.csv)" > "data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-8_fasta_analysis.csv"
 #done
 
  for j in "${!partitions_array[@]}"; do
 
   #for l in "${!sorting_method[@]}"; do
   partition=${partitions_array[j]}
-  plot_file="data-plot_mfcompress_${partitions_array[j]}.pdf"
+  plot_file="data-plot_mfcompress_${partitions_array[j]}-$SORTING_TYPE.pdf"
   #echo $plot_file
-  title="MFCOMPRESS with partition ${partitions_array[j]}"
+  title="MFCOMPRESS with partition ${partitions_array[j]}, sorted by $SORTING_TYPE"
   #gain_x=$(awk -F "\"*,\"*" '{print $8}' data_level_${levels_array[j]}.csv) 
   #cat ${level_input_file[j]}
   #point=0
@@ -106,9 +106,9 @@ cat data_mfcompress-$NAF_real_CSV-$SORTING_TYPE.csv | grep -v -e "fasta_analysis
         set multiplot layout 1,2
         count=12
       #  plot sorting_points u 7:8 w points ls count notitle
-        plot "data_mfcompress-$NAF_synthetic_CSV-$SORTING_TYPE-${partitions_array[j]}_fasta_analysis.csv" u 9:8 title "Synthetic Data" with linespoints linestyle count
+        plot "data_mfcompress-$MFCOMPRESS_synthetic_CSV-$SORTING_TYPE-${partitions_array[j]}_fasta_analysis.csv" u 9:8 title "Synthetic Data" with linespoints linestyle count
         count=count + 1
-        plot "data_mfcompress-$NAF_real_CSV-$SORTING_TYPE-${partitions_array[j]}_fasta_analysis.csv" u 9:8 title "Real Data"  with linespoints linestyle count
+        plot "data_mfcompress-$MFCOMPRESS_real_CSV-$SORTING_TYPE-${partitions_array[j]}_fasta_analysis.csv" u 9:8 title "Real Data"  with linespoints linestyle count
         count=count + 1
          
 EOF
